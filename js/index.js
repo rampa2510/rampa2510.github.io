1f58ee68a1ba3de2d29bcbbae74022cba61cad93
Ram Pandey <rampa2510@Rams-MacBook-Air.local>
2020-11-20 11:53:59 +0530

    Finished first draft

--- js/index.js
+++ js/index.js
@@ -34,3 +34,37 @@ function fadeOut(elem, ms) {
 function onLoad() {
   fadeOut(document.getElementById("preloader"), 400);
 }
+
+function scrollToSection(event, section, isMenu = false) {
+  if (supportsSmoothScrolling()) {
+    return;
+  }
+  event.preventDefault();
+  const scrollToElem = document.getElementById(section);
+  SmoothVerticalScrolling(scrollToElem, 300, "top");
+  if (isMenu) closeNav();
+}
+
+function supportsSmoothScrolling() {
+  const body = document.body;
+  const scrollSave = body.style.scrollBehavior;
+  body.style.scrollBehavior = "smooth";
+  const hasSmooth = getComputedStyle(body).scrollBehavior === "smooth";
+  body.style.scrollBehavior = scrollSave;
+  return hasSmooth;
+}
+
+function SmoothVerticalScrolling(element, time, position) {
+  var eTop = element.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+    window.setTimeout(SVS_B, curTime, eAmt, position);
+    curTime += time / 100;
+  }
+}
+
+function SVS_B(eAmt, position) {
+  if (position == "center" || position == "") window.scrollBy(0, eAmt / 2);
+  if (position == "top") window.scrollBy(0, eAmt);
+}
